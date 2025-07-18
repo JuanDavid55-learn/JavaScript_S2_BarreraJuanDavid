cbf4ba37c3d62cd0c831af789d9b0cb57a8bfde3
juan David <torresjuandavid087@gmail.com>
2025-07-18 12:26:01 -0500

    create :fire: init

---
+++ Dia1/javascript/javascript.js
@@ -0,0 +1,43 @@
+// #########################################
+// ################ dia 1 ##################
+// #########################################
+
+console.log("Hola mundo"); //comando
+variable1= 3.14; //variable
+console.log(variable1);
+console.log(typeof variable1);
+
+// suma
+palabra="Campus";
+palabra2="Lands";
+palabra3=palabra+palabra2;
+console.log(palabra3)
+
+// otro ejemplo
+nombre="juanda";
+plata=60000;
+console.log("la persona llamada ",nombre," quisiera tiener en su cuenta bancaria ",plata, " dolares");
+
+// boleanos
+boleanito= true;
+boleanito2=false;
+console.log(typeof boleanito);
+
+// condicionales
+if(boleanito == true){
+    console.log("es verdadero!!");
+}else{
+    console.log("es falso");
+}
+
+//bucles
+for(i=0;i<5;i=i+1){
+    console.log("holii");
+}
+hola = true;
+while(hola==true){
+    console.log("holi de nuevooo!!");
+    hola=false;
+}
+ 
+// Desarrollado por : Juan David Barrera Torres - T.I 1.097.101.967
