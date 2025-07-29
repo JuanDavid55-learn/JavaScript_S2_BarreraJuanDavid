9960fde717e8728a5c805d9b69e41d9aee528c2e
juan david <torresjuandavid087@gmail.com>
2025-07-29 12:44:10 -0500

    feat :fire: advance

---
+++ Dia8_trabajo_StarWars/javascript.js
@@ -0,0 +1,19 @@
+function buscarPersonaje(){
+    const xhr = new XMLHttpRequest();
+    const URL = `https://swapi.py4e.com/api/`;
+    console.log(URL)
+    xhr.open("GET",URL,true);
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState===4 && xhr.status===200){
+            try {
+                const daticos =JSON.parse(xhr.response)
+                console.log(daticos)
+            }
+            catch{
+                console.log(err.message);
+            }
+        }
+    };
+    xhr.send();
+}
+buscarPersonaje();
