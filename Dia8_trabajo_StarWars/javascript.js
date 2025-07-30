3cac588142b40d00f31e4fb967d988a32144e28b
juan david <torresjuandavid087@gmail.com>
2025-07-29 21:57:00 -0500

    feat :fire: davance 2 Dia8_trabajo_StarWars

--- Dia8_trabajo_StarWars/javascript.js
+++ Dia8_trabajo_StarWars/javascript.js
@@ -1,5 +1,27 @@
+const idUsar =prompt("ingresa el id del personaje que deseas buscar:");
+
+function buscar(){
+    const xhrho = new XMLHttpRequest();
+    const URLho = `https://swapi.py4e.com/api/planets/`;
+    xhrho.open("GET", URLho,true);
+    xhrho.onreadystatechange = function(){
+        if(xhrho.readyState===4 && xhrho.status===200){
+            try {
+                const dato =JSON.stringify(xhrho.responseText);
+                console.log(dato);
+                verhomeworld=[]
+                verhomeworld.push(dato["results"][idUsar]["name"]);
+            }
+            catch{
+                console.log(err.message);
+            }
+        }
+    };
+    xhrho.send();
+}
+buscar();
+
 function buscarPersonaje(){
-    const idUsar =prompt("ingresa el id del personaje que deseas buscar:");
     const xhr = new XMLHttpRequest();
     const URL = `https://swapi.py4e.com/api/people/`;
     console.log(URL)
@@ -12,7 +34,7 @@ function buscarPersonaje(){
             try {
                 const daticos =JSON.parse(xhr.response);
                 console.log(daticos);
-                alert("Name: "+daticos["results"][idUsar]["name"]+"\nheigt: "+daticos["results"][idUsar]["height"]+"\nmass: "+daticos["results"][idUsar]["mass"]+"\nhair_color: "+daticos["results"][idUsar]["hair_color"]+"\nskin_color: "+daticos["results"][idUsar]["skin_color"]+"\neye_color: "+daticos["results"][idUsar]["eye_color"]+"\nbirth_year: "+daticos["results"][idUsar]["birth_year"]+"\ngender: "+daticos["results"][idUsar]["gender"]);// todavia en procesooo
+                alert("Name: "+daticos["results"][idUsar]["name"]+"\nheigt: "+daticos["results"][idUsar]["height"]+"\nmass: "+daticos["results"][idUsar]["mass"]+"\nhair_color: "+daticos["results"][idUsar]["hair_color"]+"\nskin_color: "+daticos["results"][idUsar]["skin_color"]+"\neye_color: "+daticos["results"][idUsar]["eye_color"]+"\nbirth_year: "+daticos["results"][idUsar]["birth_year"]+"\ngender: "+daticos["results"][idUsar]["gender"]+"\nhomeworld: "+verhomeworld);// todavia en procesooo
             }
             catch{
                 console.log(err.message);
