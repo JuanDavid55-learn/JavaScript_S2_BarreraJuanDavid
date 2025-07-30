b7625b838c293a29b6b499baef9cdbb0a72627ba
juan david <torresjuandavid087@gmail.com>
2025-07-29 21:10:59 -0500

    feat :fire: advance Dia8_trabajo_StarWars

--- Dia8_trabajo_StarWars/javascript.js
+++ Dia8_trabajo_StarWars/javascript.js
@@ -1,18 +1,23 @@
 function buscarPersonaje(){
+    const idUsar =prompt("ingresa el id del personaje que deseas buscar:");
     const xhr = new XMLHttpRequest();
-    const URL = `https://swapi.py4e.com/api/`;
+    const URL = `https://swapi.py4e.com/api/people/`;
     console.log(URL)
     xhr.open("GET",URL,true);
     xhr.onreadystatechange = function(){
-        if(xhr.readyState===4 && xhr.status===200){
+        if(xhr.readyState===3){
+            alert("cargando...")
+        }
+        else if(xhr.readyState===4 && xhr.status===200){
             try {
-                const daticos =JSON.parse(xhr.response)
-                console.log(daticos)
+                const daticos =JSON.parse(xhr.response);
+                console.log(daticos);
+                alert("Name: "+daticos["results"][idUsar]["name"]+"\nheigt: "+daticos["results"][idUsar]["height"]+"\nmass: "+daticos["results"][idUsar]["mass"]+"\nhair_color: "+daticos["results"][idUsar]["hair_color"]+"\nskin_color: "+daticos["results"][idUsar]["skin_color"]+"\neye_color: "+daticos["results"][idUsar]["eye_color"]+"\nbirth_year: "+daticos["results"][idUsar]["birth_year"]+"\ngender: "+daticos["results"][idUsar]["gender"]);// todavia en procesooo
             }
             catch{
                 console.log(err.message);
             }
-        }
+        }        
     };
     xhr.send();
 }
