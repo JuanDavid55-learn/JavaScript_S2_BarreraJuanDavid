736153709ae243bb6318a0095e0488f7db567e33
juan david <torresjuandavid087@gmail.com>
2025-08-02 00:37:34 -0500

    feat :fire: enter rules poker royal table

---
+++ Dia10/script.js
@@ -0,0 +1,9 @@
+// botones del html
+let reglitas = document.getElementById("verReglas");
+reglitas.addEventListener("click",function (){
+    document.getElementById("reglas").style.display= "block";
+})
+let salirReglitas = document.getElementById("salirReglas");
+salirReglitas.addEventListener("click",function (){
+    document.getElementById("reglas").style.display= "none";
+})
