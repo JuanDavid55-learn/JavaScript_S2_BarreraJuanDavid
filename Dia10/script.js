7183f620382adeb4d0b1e31834230cebae1f6653
juan david <torresjuandavid087@gmail.com>
2025-08-02 22:11:49 -0500

    feat :fire: game table

--- Dia10/script.js
+++ Dia10/script.js
@@ -7,3 +7,11 @@ let salirReglitas = document.getElementById("salirReglas");
 salirReglitas.addEventListener("click",function (){
     document.getElementById("reglas").style.display= "none";
 })
+let entrarJugar = document.getElementById("jugar");
+entrarJugar.addEventListener("click",function (){
+    document.getElementById("tableroJuego").style.display= "block";
+})
+let salirPartida = document.getElementById("salirDeParida");
+salirPartida.addEventListener("click",function (){
+    document.getElementById("tableroJuego").style.display= "none";
+})
