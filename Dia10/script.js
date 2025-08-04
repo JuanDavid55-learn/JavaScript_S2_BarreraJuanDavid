c11895a423d0194a4a5c3725b461a1488dcaa59d
juan david <torresjuandavid087@gmail.com>
2025-08-04 14:00:20 -0500

    feat :fire: advance 2 day 10

--- Dia10/script.js
+++ Dia10/script.js
@@ -15,14 +15,33 @@ let salirPartida = document.getElementById("salirDeParida");
 salirPartida.addEventListener("click",function (){
     document.getElementById("tableroJuego").style.display= "none";
 })
-
 let repartircartas = document.getElementById("botondeMazo");
 repartircartas.addEventListener("click", function () {
     document.getElementById("pilaDeManoE").style.visibility= "visible";
     document.getElementById("pilaDeManoJ").style.visibility= "visible";
     document.getElementById("datosPartida").style.visibility = "visible";
     document.getElementById("textreparte").style.display = "none";
+    for(i=0;i<2;i=i+1){
+        if(i==0){
+            for(j=0;j<4;j=j+1){
+                setTimeout(cartAlAzar);
+                listCartasMano.push(CartasTem [0]);
+                const eliminar = todasLasCartas.indexOf(CartasTem[0])
+                if(eliminar !== -1){
+                    todasLasCartas.splice(eliminar,1);
+                }
+            }
+        }
+    }
 });
+CartasTem.push(cartAlAzar());
+console.log(listCartasMano);
+console.log(CartasTem);
+
+let verManito = document.getElementById("pilaDeManoJ");
+verManito.addEventListener("click",function (){
+    document.getElementById("vercartasdemano").style.display= "block";
+})
 
 //crear las cartas
 let todasLasCartas=[];
@@ -248,3 +267,54 @@ for(i=0;i<4;i=i+1){
     }
 }
 console.log(todasLasCartas);
+
+// funciones con cartas
+let listCartasMano=[];
+let listCartasManoE=[];
+let listCartasHINDR=[];
+let listCartasHINDRE=[];
+let CartasTem=[];
+
+function cartAlAzar (){
+    return todasLasCartas[Math.floor(Math.random()* todasLasCartas.length)];
+}
+
+function vercartasDeMano() {
+    document.getElementById("vercartasdemano").innerHTML=``;
+    const idCartas = JSON.stringify(todasLasCartas);
+    console.log(idCartas);
+    const xhr = new XMLHttpRequest();
+    const url = `https://deckofcardsapi.com/static/img/AC.png${idCartas}`;
+    console.log(url);
+    xhr.open("GET", url, true);
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 3) {
+
+            console.log("Cargando...");
+        }
+        else if (xhr.readyState === 4 && xhr.status === 200) {
+            try {
+                const daticos = JSON.parse(xhr.responseText);
+                if (daticos.results && daticos.results.length > 0) {
+                    for (let i = 0; i < daticos.results.length; i++) {
+                        let division = document.getElementById("resultados");
+                        division.innerHTML += `
+                        <div class="card">
+                        <img src="${daticos["results"][i]["image"]}" alt="">
+                        <h3>${daticos["results"][i]["name"]}</h3>
+                        <p><strong>Status:</strong>${daticos["results"][i]["status"]}</p>
+                        <p><strong>Specie:</strong>${daticos["results"][i]["species"]}</p>
+                        </div>
+                        `
+                        console.log(daticos["results"][i]["name"]);
+                    }
+                };
+            }
+            catch (err) {
+                console.log(err.message); //Si algo malo pasa, imprima el error
+            }
+        }
+    };
+    xhr.send();
+};
