1746874447bdaa1465300142ab66aef259fbb711
juan david <torresjuandavid087@gmail.com>
2025-08-08 21:18:59 -0500

    feat: :fire: advance 7 day 10

--- Dia10/script.js
+++ Dia10/script.js
@@ -20,7 +20,6 @@ repartirYEmpezar.addEventListener("click", function () {
     document.getElementById("pilaDeManoE").style.visibility= "visible";
     document.getElementById("pilaDeManoJ").style.visibility= "visible";
     document.getElementById("textreparte").style.display = "none";
-    laRonda();
     for (let i = 0; i < 6; i=i+1) {
         listCartasMano.push(cartaAlAzar());
         listCartasManoE.push(cartaAlAzar());
@@ -301,43 +300,26 @@ function cartaAlAzar() {
     return cartaTem;
 }
 
-// rondas juego jugador
-let RondaNuero=0;
-let numronda =document.getElementById("rondas")
-numronda =document.getElementById("rondas").innerHTML +=`${RondaNuero}`
+// rondas juego jugadores
+let RondaNuero=1;
 
-function laRonda(){
-    RondaNuero=RondaNuero+1;
-}
-for (let i=1;i<41;i=i+1){
-    if(i===40){
-    }
-    else if (i%15===0){
-        i=i+1
-    }
-    else if (i%3===0){
-        i=i+1
-    }
-    else if (i%5===0){
-        i=i+1
-    }
-    else if (i%2===0){
-        i=i+1
-    }
-    else if(i%1===0){
-        function rondasComunes(){
-            
-        }
-    }
-}
+let EmpezarRondas = document.getElementById("botondeMazo");
+EmpezarRondas.addEventListener("click", function () {
+    numronda =document.getElementById("rondas")
+    numronda =document.getElementById("rondas").innerHTML +=`${RondaNuero}`
+    document.getElementById("movHINDR").style.display = "block";
+});
 
-// funciones de cartas de la mano
+// funciones de las cartas de las mano
 let carta1 = document.getElementById("deHindr1");
 carta1.addEventListener("click", function () {
 listCartasHINDR.push(listCartasMano[0]);
 listCartasMano.splice(0,1);
 document.getElementById("botonesdemoverh").style.display = "none";
 document.getElementById("pilaHINDRJ").style.visibility = "visible";
+document.getElementById("ultimaCarta").style.visibility = "hidden";
+document.getElementById("movHINDR").style.display = "none";
+document.getElementById("robarcartica").style.display = "block";
 console.log("pila hindr",listCartasHINDR);
 });
 let carta2 = document.getElementById("deHindr2");
@@ -346,6 +328,9 @@ listCartasHINDR.push(listCartasMano[1]);
 listCartasMano.splice(1,1);
 document.getElementById("botonesdemoverh").style.display = "none";
 document.getElementById("pilaHINDRJ").style.visibility = "visible";
+document.getElementById("ultimaCarta").style.visibility = "hidden";
+document.getElementById("movHINDR").style.display = "none";
+document.getElementById("robarcartica").style.display = "block";
 console.log("pila hindr",listCartasHINDR);
 });
 let carta3 = document.getElementById("deHindr3");
@@ -354,6 +339,9 @@ listCartasHINDR.push(listCartasMano[2]);
 listCartasMano.splice(2,1);
 document.getElementById("botonesdemoverh").style.display = "none";
 document.getElementById("pilaHINDRJ").style.visibility = "visible";
+document.getElementById("ultimaCarta").style.visibility = "hidden";
+document.getElementById("movHINDR").style.display = "none";
+document.getElementById("robarcartica").style.display = "block";
 console.log("pila hindr",listCartasHINDR);
 });
 let carta4 = document.getElementById("deHindr4");
@@ -362,6 +350,9 @@ listCartasHINDR.push(listCartasMano[3]);
 listCartasMano.splice(3,1);
 document.getElementById("botonesdemoverh").style.display = "none";
 document.getElementById("pilaHINDRJ").style.visibility = "visible";
+document.getElementById("ultimaCarta").style.visibility = "hidden";
+document.getElementById("movHINDR").style.display = "none";
+document.getElementById("robarcartica").style.display = "block";
 console.log("pila hindr",listCartasHINDR);
 });
 let carta5 = document.getElementById("deHindr5");
@@ -370,6 +361,9 @@ listCartasHINDR.push(listCartasMano[4]);
 listCartasMano.splice(4,1);
 document.getElementById("botonesdemoverh").style.display = "none";
 document.getElementById("pilaHINDRJ").style.visibility = "visible";
+document.getElementById("ultimaCarta").style.visibility = "hidden";
+document.getElementById("movHINDR").style.display = "none";
+document.getElementById("robarcartica").style.display = "block";
 console.log("pila hindr",listCartasHINDR);
 });
 let carta6 = document.getElementById("deHindr6");
@@ -378,5 +372,25 @@ listCartasHINDR.push(listCartasMano[5]);
 listCartasMano.splice(5,1);
 document.getElementById("botonesdemoverh").style.display = "none";
 document.getElementById("pilaHINDRJ").style.visibility = "visible";
+document.getElementById("ultimaCarta").style.visibility = "hidden";
+document.getElementById("movHINDR").style.display = "none";
+document.getElementById("robarcartica").style.display = "block";
 console.log("pila hindr",listCartasHINDR);
+});
+
+let robarCartica = document.getElementById("robarcartica");
+robarCartica.addEventListener("click", function () {
+    listCartasMano.push(cartaAlAzar());
+    document.getElementById("ultimaCarta").style.visibility = "visible";
+    console.log("(haz robado una carta) tus cartas:",listCartasMano);
+});
+let finalizarYEmpezarNueva = document.getElementById("robarcartica");
+finalizarYEmpezarNueva.addEventListener("click", function () {
+    document.getElementById("robarcartica").style.display = "none";
+    RondaNuero=RondaNuero+1;
+    numronda =document.getElementById("rondas").innerHTML =`<p>RONDA:</p>`
+    numronda =document.getElementById("rondas").innerHTML +=`${RondaNuero}`
+    document.getElementById("movHINDR").style.display = "block";
+    console.log("cartas de mazo:",todasLasCartas);
+    console.log("tus cartas:",listCartasMano);
 });
